cbcd1a70c3d560d0d5374ec8f7d5d10993f1f718
bhil3d <weberson.v10@gmail.com>
2019-08-19 00:19:51 -0400

    corredorNorte primeiro teste df

---
+++ app/src/main/java/com/example/corredor/Activity/Tela_df_resultados.java
@@ -0,0 +1,17 @@
+package com.example.corredor.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.corredor.R;
+
+public class Tela_df_resultados extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_df_resultados);
+    }
+}
---
+++ app/src/main/java/com/example/corredor/Class/ClassDF.java
@@ -0,0 +1,166 @@
+package com.example.corredor.Class;
+
+import java.io.Serializable;
+
+public class ClassDF implements Serializable {
+
+    private String IdDf;
+    private String spd11dia;
+    private String spd11semana;
+    private String sd11mes;
+    private String spd11ano;
+    private String spretrodia;
+    private String spretrosemana;
+    private String spretromes;
+    private String spretroano;
+    private String sppatroldia;
+    private String sppatrolsemana;
+    private String sppatrolmes;
+    private String sppatrolano;
+    private String spperfuratrizdia;
+    private String spperfuratrizsemana;
+    private String spperfuratrizmes;
+    private String spperfuratrizano;
+
+
+    public ClassDF() {
+
+    }
+
+
+    public String getIdDf() {
+        return IdDf;
+    }
+
+    public void setIdDf(String idDf) {
+        IdDf = idDf;
+    }
+
+    public String getSpd11dia() {
+        return spd11dia;
+    }
+
+    public void setSpd11dia(String spd11dia) {
+        this.spd11dia = spd11dia;
+    }
+
+    public String getSpd11semana() {
+        return spd11semana;
+    }
+
+    public void setSpd11semana(String spd11semana) {
+        this.spd11semana = spd11semana;
+    }
+
+    public String getSd11mes() {
+        return sd11mes;
+    }
+
+    public void setSd11mes(String sd11mes) {
+        this.sd11mes = sd11mes;
+    }
+
+    public String getSpd11ano() {
+        return spd11ano;
+    }
+
+    public void setSpd11ano(String spd11ano) {
+        this.spd11ano = spd11ano;
+    }
+
+    public String getSpretrodia() {
+        return spretrodia;
+    }
+
+    public void setSpretrodia(String spretrodia) {
+        this.spretrodia = spretrodia;
+    }
+
+    public String getSpretrosemana() {
+        return spretrosemana;
+    }
+
+    public void setSpretrosemana(String spretrosemana) {
+        this.spretrosemana = spretrosemana;
+    }
+
+    public String getSpretromes() {
+        return spretromes;
+    }
+
+    public void setSpretromes(String spretromes) {
+        this.spretromes = spretromes;
+    }
+
+    public String getSpretroano() {
+        return spretroano;
+    }
+
+    public void setSpretroano(String spretroano) {
+        this.spretroano = spretroano;
+    }
+
+    public String getSppatroldia() {
+        return sppatroldia;
+    }
+
+    public void setSppatroldia(String sppatroldia) {
+        this.sppatroldia = sppatroldia;
+    }
+
+    public String getSppatrolsemana() {
+        return sppatrolsemana;
+    }
+
+    public void setSppatrolsemana(String sppatrolsemana) {
+        this.sppatrolsemana = sppatrolsemana;
+    }
+
+    public String getSppatrolmes() {
+        return sppatrolmes;
+    }
+
+    public void setSppatrolmes(String sppatrolmes) {
+        this.sppatrolmes = sppatrolmes;
+    }
+
+    public String getSppatrolano() {
+        return sppatrolano;
+    }
+
+    public void setSppatrolano(String sppatrolano) {
+        this.sppatrolano = sppatrolano;
+    }
+
+    public String getSpperfuratrizdia() {
+        return spperfuratrizdia;
+    }
+
+    public void setSpperfuratrizdia(String spperfuratrizdia) {
+        this.spperfuratrizdia = spperfuratrizdia;
+    }
+
+    public String getSpperfuratrizsemana() {
+        return spperfuratrizsemana;
+    }
+
+    public void setSpperfuratrizsemana(String spperfuratrizsemana) {
+        this.spperfuratrizsemana = spperfuratrizsemana;
+    }
+
+    public String getSpperfuratrizmes() {
+        return spperfuratrizmes;
+    }
+
+    public void setSpperfuratrizmes(String spperfuratrizmes) {
+        this.spperfuratrizmes = spperfuratrizmes;
+    }
+
+    public String getSpperfuratrizano() {
+        return spperfuratrizano;
+    }
+
+    public void setSpperfuratrizano(String spperfuratrizano) {
+        this.spperfuratrizano = spperfuratrizano;
+    }
+}
